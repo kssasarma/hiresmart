1354ae218354dea59daa2f1ee0326aceb4672c42
Satya Kodamanchili <karavindasar@opentext.com>
2025-03-27 00:04:43 +0530

    🐛 Routes fixed for context deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import Navbar from './components/Navbar';
-import CandidateDashboard from './pages/CandidateDashboard';
-import RecruiterDashboard from './pages/RecruiterDashboard';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar";
+import CandidateDashboard from "./pages/CandidateDashboard";
+import RecruiterDashboard from "./pages/RecruiterDashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <div className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/candidate" element={<CandidateDashboard />} />
-            <Route path="/recruiter" element={<RecruiterDashboard />} />
-          </Routes>
-        </div>
-        <Toaster position="top-right" />
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter basename="/hiresmart">
+			<div className="min-h-screen bg-gray-50">
+				<Navbar />
+				<div className="container mx-auto px-4 py-8">
+					<Routes>
+						<Route path="/" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+						<Route path="/candidate" element={<CandidateDashboard />} />
+						<Route path="/recruiter" element={<RecruiterDashboard />} />
+					</Routes>
+				</div>
+				<Toaster position="top-right" />
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
